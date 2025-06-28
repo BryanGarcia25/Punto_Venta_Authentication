3c8d3eaa6526df81b780c718d33341b778d8567e
Bryan Garcia <osvaldo52programacion@gmail.com>
2025-06-28 01:17:57 -0700

    first commit

---
+++ src/main/java/com/smartposlocal/mx/MxApplication.java
@@ -0,0 +1,13 @@
+package com.smartposlocal.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxApplication.class, args);
+	}
+
+}
